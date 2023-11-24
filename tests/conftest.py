9721495efb14b482a0de4ac60a95a850fa0ea9f6
sergeyvasilev <sergeyvasilev251@gmail.com>
2023-11-24 16:06:18 +0500

    1_Поменял путь к файлу "PNG" . 2_Результирующую проверку поправил.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,18 @@
 import pytest
 from selene import browser
+from selenium import webdriver
 
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
     browser.config.base_url = 'https://demoqa.com'
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
-    browser.config.browser_name = 'chrome'
+    driver_options = webdriver.ChromeOptions()
+    # driver_options.add_argument('--headless')
+    browser.config.driver_options = driver_options
+    browser.config.timeout = 5
+    browser.config.window_height = 1920
+    browser.config.window_width = 1080
+
     yield
+
     browser.quit()
