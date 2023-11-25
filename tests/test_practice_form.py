edef89e69dc5e53fc4de61950cf1aa3978e62ed6
sergeyvasilev <sergeyvasilev251@gmail.com>
2023-11-25 17:31:28 +0500

    Пофиксил

--- tests/test_practice_form.py
+++ tests/test_practice_form.py
@@ -1,7 +1,6 @@
 from selene import browser, have, be
-from pathlib import Path
 from tests import resources
-
+import os.path
 
 
 def test_registration_form():
@@ -19,7 +18,8 @@ def test_registration_form():
     browser.element("#subjectsInput").set_value('Chemistry').press_enter()
     browser.element('[for=hobbies-checkbox-1]').click()
     browser.element('#uploadPicture').set_value(
-        str(Path(resources.__file__).joinpath('resources/python_lesson_5.png').absolute()))
+        (os.path.join(os.path.dirname(resources.__file__), 'screen.png'))
+    )
 
     browser.element('#currentAddress').type('Test,Test,13')
     browser.element('#react-select-3-input').type('NCR').press_enter()
@@ -27,19 +27,19 @@ def test_registration_form():
     browser.element('#submit').press_enter()
     browser.element('.modal-header').should(have.text('Thanks for submitting the form'))
 
-
     browser.element('.table').all('td').should(
-    have.texts(
-        ('Student Name', 'Test Test'),
-        ('Student Email', 'Test@test.com'),
-        ('Gender', 'Male'),
-        ('Mobile', '8999999999'),
-        ('Date of Birth', '11 November,1991'),
-        ('Subjects', 'Chemistry'),
-        ('Hobbies', 'Sports'),
-        ('Picture', 'python_lesson_5.png'),
-        ('Address', 'Test,Test,13'),
-        ('State and City', 'NCR Delhi'),
+        have.texts(
+            ('Student Name', 'Test Test'),
+            ('Student Email', 'Test@test.com'),
+            ('Gender', 'Male'),
+            ('Mobile', '8999999999'),
+            ('Date of Birth', '11 November,1991'),
+            ('Subjects', 'Chemistry'),
+            ('Hobbies', 'Sports'),
+            ('Picture', 'screen.png'),
+            ('Address', 'Test,Test,13'),
+            ('State and City', 'NCR Delhi'),
+        )
     )
-)
-browser.element('#closeLargeModal').press_enter()
+
+    browser.element('#closeLargeModal').press_enter()
